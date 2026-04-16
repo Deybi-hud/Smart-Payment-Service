95d50be4c73890f8550016b6babcf7c59d5c44bc
deybi-hud <hu4nt4r@gmail.com>
2026-04-16 16:20:13 -0400

    Inicio

---
+++ src/main/java/com/smart_solution/api_payment_service/ApiPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smart_solution.api_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart_solution/api_payment_service/ApiPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_solution.api_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
